5a88929a7755f970d2b49f95ba7b80ebe1341ebe
Julia Edwards <jedwardsjunior@gmail.com>
2015-01-27 00:09:02 -0500

    Font

--- server.js
+++ server.js
@@ -25,8 +25,21 @@ app.set('view engine', 'jade');
 
 // route ===============================================
 
+var d = new Date();
+var weekday = new Array(7);
+weekday[0]=  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var day = weekday[d.getDay()+1];
+
 var answer = "Yes!";
-var today = "Classes are canceled on Tuesday"
+var today = "Classes are canceled on ".concat(day);
+
 
 app.get('/', function(req, res){
   res.render('index',
