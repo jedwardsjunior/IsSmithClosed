43ce8a87b93d17de4a984b57cc63cd77380db27c
Julia Edwards <jedwardsjunior@gmail.com>
2015-01-26 23:49:18 -0500

    Snow day

---
+++ server.js
@@ -0,0 +1,42 @@
+// modules =================================================
+var express        = require('express');
+var app            = express();
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+
+// configuration ===========================================
+var port = process.env.PORT || 8080; // set our port
+
+
+// setup middleware
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+
+
+// Set our default template engine to "jade"
+// which prevents the need for extensions
+// (although you can still mix and match)
+app.set('views', __dirname + '/public/views');
+app.set('view engine', 'jade');
+
+// route ===============================================
+
+var answer = "Yes!";
+var today = "Classes are cancelled on Tuesday"
+
+app.get('/', function(req, res){
+  res.render('index',
+            { "snowday": answer,
+              "day"    : today
+            });
+});
+
+
+// start app ===============================================
+app.listen(port);
+console.log('Magic happens on port ' + port); 			// shoutout to the user
+exports = module.exports = app; 						// expose app
